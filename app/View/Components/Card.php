4f73906e50e0e75f059178c7cc5be90fbcef8fd6
wahyunup <wahyunurpambuko@gmail.com>
2024-11-17 16:37:34 +0700

    update : fitureUsecase KTNA

--- app/View/Components/Card.php
+++ app/View/Components/Card.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class Products extends Component
+class Card extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class Products extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.products');
+        return view('components.card');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    return view('home');
+    return view('pages.home');
 });
 
-Route::get('/product', function () {
-    return view('product');
-});
+Route::get('/product', [ProductController::class, 'showProducts'])->name('products');
+
+Route::get('/productdetail/{id}', [ProductController::class, 'showProductdetails'])->name('products.show');
 
 Route::fallback(function () {
-    return response()->view('errors404', [], 404);
+    return response()->view('pages.errors404', [], 404);
 });
